44f6606a3b3de9445a59ab4d6b423026b38c0140
shakti sumant das <shakti.20k@gmail.com>
2024-12-09 23:41:06 +0530

    setup cloudinary and multer for image upload

--- server/controllers/admin/product.controller.js
+++ server/controllers/admin/product.controller.js
@@ -1,4 +1,24 @@
+const { imageUploadUtil } = require("../../helper/cloudinary");
 const Product = require("../../models/product.model");
+
+const handleImageUpload = async (req, res) => {
+  try {
+    const b64 = Buffer.from(req.file.buffer).toString("base64");
+    const url = "data:" + req.file.mimetype + ";base64," + b64;
+    const result = await imageUploadUtil(url);
+
+    return res.status(200).json({
+      success: true,
+      result,
+    });
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: "Error occured",
+    });
+  }
+};
 
 const addProduct = async (req, res) => {
   try {
@@ -141,4 +161,5 @@ module.exports = {
   fetchAllProducts,
   editProduct,
   deleteProduct,
+  handleImageUpload,
 };
--- server/routes/admin/product.route.js
+++ server/routes/admin/product.route.js
@@ -3,14 +3,16 @@ const {
     addProduct, 
     editProduct, 
     deleteProduct, 
-    fetchAllProducts 
+    fetchAllProducts, 
+    handleImageUpload
 } = require("../../controllers/admin/product.controller");
+const { upload } = require("../../helper/cloudinary");
 
 
 
 const router = express.Router();
 
-//router.post("/upload-image", upload.single("my_file"), handleImageUpload);
+router.post("/upload-image", upload.single("my_file"), handleImageUpload);
 router.post("/add", addProduct);
 router.put("/edit/:id", editProduct);
 router.delete("/delete/:id", deleteProduct);
