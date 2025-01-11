b9b85c6260fe3ee9120469b11a78e78601f613bf
shakti sumant das <shakti.20k@gmail.com>
2025-01-11 15:22:36 +0530

    update cors origin

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: [process.env.CLIENT_BASE_URL || 'http://localhost:5173'],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
@@ -38,6 +38,7 @@ app.use(
     credentials: true,
   })
 );
+
 
 //routes
 app.use("/api/auth", authRoute);
